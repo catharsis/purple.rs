df7e7e738b8b6d55994d98c54a1940bf9fa94580
Anton Lofgren <anton.lofgren@gmail.com>
2014-07-14 21:29:42 +0200

    Make it possible to start purple
    Signed-off-by: Anton Lofgren <anton.lofgren@gmail.com>

--- src/ll.rs
+++ src/ll.rs
@@ -1,5 +1,37 @@
+#![allow(non_camel_case_types)]
+#![allow(dead_code)]
+extern crate libc;
+use self::libc::{c_int,c_uint, c_void, c_char};
+
+pub type gboolean = bool;
+pub type guint = c_uint;
+pub type gpointer = *mut c_void; //FIXME, (void *)
+pub type GSourceFunc = fn(user_data:gpointer) -> gboolean; //FIXME, investigate
+pub type PurpleInputFunction = (); //FIXME: investigate
+pub type PurpleInputCondition = (); //FIXME: bitmask
+pub struct PurpleEventLoopUiOps {
+	pub timeout_add: fn(interval:guint, function:GSourceFunc, data:gpointer) -> c_int,
+	pub timeout_remove: fn(handle:guint) -> gboolean,
+	pub input_add: fn(_:c_int, _:PurpleInputCondition, _:PurpleInputFunction, _:gpointer) -> guint,
+	pub input_remove: fn(_:guint) -> gboolean,
+	pub input_get_error: Option<fn(_:c_int, _:*mut c_int) -> c_int>,
+	pub timeout_add_seconds: Option<fn(_:guint, _:GSourceFunc) -> guint>
+}
+
 #[link(name="purple")]
 extern {
 	pub fn purple_debug_set_enabled(_:bool) -> ();
 	pub fn purple_debug_is_enabled() -> bool;
+}
+
+#[link(name="purple")]
+extern {
+	pub fn purple_core_init(_: *const c_char) -> ();
+	pub fn purple_core_get_ui() -> *const c_char;
+}
+
+#[link(name="purple")]
+extern {
+	pub fn purple_eventloop_set_ui_ops(_: *mut PurpleEventLoopUiOps) -> ();
+	pub fn purple_eventloop_get_ui_ops() -> *mut PurpleEventLoopUiOps;
 }
--- src/purple.rs
+++ src/purple.rs
@@ -1,7 +1,9 @@
 #![crate_name = "purple"]
 #![crate_type = "lib"]
 #![feature(globs)]
+extern crate libc;
 use self::ll::*;
+use std::c_str::CString;
 pub mod ll;
 pub fn debug_set_enabled(debug: bool) -> () {
 	unsafe {
@@ -15,6 +17,31 @@ pub fn debug_is_enabled() -> bool {
 	}
 }
 
+pub fn core_init(ui: &str) -> () {
+	unsafe {
+		let cstr = ui.to_c_str();
+		purple_core_init(cstr.as_ptr());
+	}
+}
+
+pub fn core_get_ui() -> String {
+	unsafe {
+		CString::new(purple_core_get_ui(), false).as_str().unwrap().to_string()
+	}
+}
+
+pub fn eventloop_set_ui_ops(ops: *mut PurpleEventLoopUiOps) -> () {
+	unsafe {
+		purple_eventloop_set_ui_ops(ops);
+	}
+}
+
+pub fn eventloop_get_ui_ops() -> *mut PurpleEventLoopUiOps {
+	unsafe {
+		purple_eventloop_get_ui_ops()
+	}
+}
+
 #[test]
 fn test_debug() {
 	let d = debug_is_enabled();
@@ -22,4 +49,42 @@ fn test_debug() {
 	debug_set_enabled(true);
 	let d = debug_is_enabled();
 	assert!(d == true);
+}
+
+#[test]
+fn test_core_init() {
+	// FIXME: Woo... Bring out your boilerplates
+	use self::libc::c_int;
+	let e = "PURPLE_TEST";
+	fn timeout_add_fn(_: guint, _: GSourceFunc, _: gpointer) -> c_int {
+		println!("timeout_add_fn called");
+		0
+	};
+
+	fn timeout_remove_fn(_: guint) -> gboolean {
+		println!("timeout_remove_fn called");
+		true
+	};
+
+	fn input_add_fn(_:c_int, _:PurpleInputCondition, _:PurpleInputFunction, _:gpointer) -> guint {
+		println!("input_add_fn called");
+		0
+	};
+
+	fn input_remove_fn(_:guint) -> gboolean {
+		println!("input_remove called");
+		true
+	};
+
+	let mut ops = PurpleEventLoopUiOps{
+		timeout_add: timeout_add_fn,
+		timeout_remove: timeout_remove_fn,
+		input_add: input_add_fn,
+		input_remove: input_remove_fn,
+		input_get_error: None,
+		timeout_add_seconds: None,
+	};
+	eventloop_set_ui_ops(&mut ops);
+	core_init(e);
+	assert!(e == core_get_ui().as_slice());
 }
